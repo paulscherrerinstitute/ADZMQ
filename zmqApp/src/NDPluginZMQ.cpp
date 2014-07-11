2f292d8369bf8855f58dd1831aa0b1056d9efe5a
wang_x1 <xiaoqiang.wang@psi.ch>
2014-07-11 23:08:40 +0200

    be specific to maxAddr and NUM_PARAM

--- zmqApp/src/NDPluginZMQ.cpp
+++ zmqApp/src/NDPluginZMQ.cpp
@@ -196,7 +196,7 @@ NDPluginZMQ::NDPluginZMQ(const char *portName, const char* serverHost, int queue
      * This driver can block (because writing a file can be slow), and it is not multi-device.  
      * Set autoconnect to 1.  priority and stacksize can be 0, which will use defaults. */
     : NDPluginDriver(portName, queueSize, blockingCallbacks, 
-                     NDArrayPort, NDArrayAddr, maxAddr, NUM_NDPLUGIN_ZMQ_PARAMS, maxBuffers, maxMemory, 
+                     NDArrayPort, NDArrayAddr, 1, 0, maxBuffers, maxMemory, 
                      asynGenericPointerMask, asynGenericPointerMask,
                      0, 1, priority, stackSize)
 {
