9373984fc02e31d029a2c77b05feea8f3ee31886
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2014-07-16 13:50:35 +0200

    add options for socket address and type

--- tests/zmq_server.py
+++ tests/zmq_server.py
@@ -3,10 +3,26 @@ from __future__ import print_function
 import time
 import zmq
 import numpy
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--type', dest='stype', type=str, default='PUB')
+parser.add_argument('--host', dest='host', type=str, default='tcp://*:5432')
+
+args = parser.parse_args()
+if args.stype == 'PUB':
+    stype = zmq.PUB
+elif args.stype == 'PUSH':
+    stype = zmq.PUSH
+else:
+    stype = zmq.PUB
+    print("Unsupported socket type %s, use PUB"%args.stype)
+
+print("Server %s %s"%(args.host, args.stype))
 
 context = zmq.Context()
-sock = context.socket(zmq.PUB)
-sock.bind('tcp://*:5432')
+sock = context.socket(stype)
+sock.bind(args.host)
 
 cols = 800
 rows = 600
